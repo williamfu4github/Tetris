2aa9c794f9d2381d3570c061d7fca32024349251
William Fu <williamfu4github@gmail.com>
2018-11-20 20:34:21 -0500

    add class TetrisBoard

---
+++ Tetris/Include/Model/TetrisBoard.hpp
@@ -0,0 +1,30 @@
+#ifndef TetrisBoard_hpp
+#define TetrisBoard_hpp
+
+#include "Utility/Position.hpp"
+#include "Model/Tetromino/TetrominoType.hpp"
+#include <vector>
+
+class TetrisBoard {
+public:
+    TetrisBoard();
+    ~TetrisBoard();
+    bool checkTilesEmpty(std::vector<Position> tilePositions) const;
+    bool checkTilesOutside(std::vector<Position> tilePositions) const;
+    void setTilesState(std::vector<Position> tilePositions, TetrominoType newState);
+    int countFullRows() const;    // not used
+    void clearFullRows();
+private:
+    bool checkOneTileEmpty(Position tilePosition) const;
+    
+private:
+    std::vector<std::vector<TetrominoType>> gameBoard;
+    std::vector<int> rowOccupancies;
+    
+private:
+    static const int boardSizeRow;
+    static const int boardSizeColumn;
+    static const int boardBoundaryRow;
+};
+
+#endif
---
+++ Tetris/Source/Model/TetrisBoard.cpp
@@ -0,0 +1,89 @@
+#include "Model/TetrisBoard.hpp"
+
+using std::vector;
+
+const int TetrisBoard::boardSizeRow = 25;
+const int TetrisBoard::boardSizeColumn = 10;
+const int TetrisBoard::boardBoundaryRow = 20;
+
+TetrisBoard::TetrisBoard() {
+    for (int row = 0; row < TetrisBoard::boardSizeRow; row ++) {
+        gameBoard.push_back(vector<TetrominoType>{});
+        gameBoard.back().resize(TetrisBoard::boardSizeColumn, TetrominoType::EMPTY);
+        rowOccupancies.push_back(0);
+    }
+}
+
+TetrisBoard::~TetrisBoard() {
+}
+
+// NOTE: position may be off board
+bool TetrisBoard::checkTilesEmpty(vector<Position> tilePositions) const {
+    for (Position position : tilePositions) {
+        if (!this->checkOneTileEmpty(position)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// PRE: position must be on board
+bool TetrisBoard::checkTilesOutside(vector<Position> tilePositions) const {
+    for (Position position : tilePositions) {
+        if (position.row < TetrisBoard::boardBoundaryRow) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// PRE: position must be on board
+// PRE: no duplicate positions
+void TetrisBoard::setTilesState(vector<Position> tilePositions, TetrominoType newState) {
+    for (Position position : tilePositions) {
+        TetrominoType oldState = gameBoard[position.row][position.column];
+        if ((oldState == TetrominoType::EMPTY) && (newState != TetrominoType::EMPTY)) {
+            rowOccupancies[position.row] ++;
+        }
+        else if ((oldState != TetrominoType::EMPTY) && (newState == TetrominoType::EMPTY)) {
+            rowOccupancies[position.row] --;
+        }
+        gameBoard[position.row][position.column] = newState;
+    }
+}
+
+int TetrisBoard::countFullRows() const {
+    int count = 0;
+    for (int row = 0; row < TetrisBoard::boardSizeRow; row ++) {
+        if (rowOccupancies[row] == TetrisBoard::boardSizeColumn) {
+            count ++;
+        }
+    }
+    return count;
+}
+
+void TetrisBoard::clearFullRows() {
+    for (int row = TetrisBoard::boardSizeRow - 1; row >= 0; row --) {
+        if (rowOccupancies[row] == TetrisBoard::boardSizeColumn) {
+            for (int index = row + 1; index < TetrisBoard::boardSizeRow; index ++) {
+                // use swap to improve time efficiency to O(1)
+                std::swap(gameBoard[index - 1], gameBoard[index]);
+                rowOccupancies[index - 1] = rowOccupancies[index];
+            }
+            gameBoard.back().clear();
+            gameBoard.back().resize(TetrisBoard::boardSizeColumn, TetrominoType::EMPTY);
+            rowOccupancies.back() = 0;
+        }
+    }
+}
+
+// NOTE: position may be off board
+bool TetrisBoard::checkOneTileEmpty(Position tilePosition) const {
+    if ((tilePosition.row < 0) || (tilePosition.row >= TetrisBoard::boardSizeRow)) {
+        return false;
+    }
+    if ((tilePosition.column < 0) || (tilePosition.column >= TetrisBoard::boardSizeColumn)) {
+        return false;
+    }
+    return (gameBoard[tilePosition.row][tilePosition.column] == TetrominoType::EMPTY);
+}
